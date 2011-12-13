c0dc2e58c98e6bfa198928d3c420500d9e6808b4
James Simpson <js2310@doc.ic.ac.uk>
2011-12-13 12:33:55 +0000

    Type checking for assignments

--- main.hs
+++ main.hs
@@ -12,7 +12,7 @@ main = do
 
 ------------------------------ Symbol Table ------------------------------------
 
-type SymbolTableEntry = (String, (MType, Exp))
+type SymbolTableEntry = (String,MType)
 type SymbolTable = [SymbolTableEntry]
 
 getInstr :: AST -> [Statement]
@@ -31,15 +31,26 @@ buildSymbolTable currentST ((Assign name val):statements) =
     if lookup name currentST == Nothing
 	then error ("Oh No! You can't go using " ++ name ++ 
                 " without telling me what it is first!")
-    else buildSymbolTable (updateSymbolTable currentST
-                     (name, ((fst (fromJust (lookup name currentST))), val)))
-                     statements
+--    else buildSymbolTable (updateSymbolTable currentST
+--                     (name, ((fst (fromJust (lookup name currentST))))))
+--                     statements
+    else (
+            if checkType val (fromJust (lookup name currentST))
+            then buildSymbolTable currentST statements
+            else error "Type error!"
+         )
 buildSymbolTable currentST ((Declare typ name):statements) =
     if lookup name currentST == Nothing
-    then buildSymbolTable ((name, (typ, Int 0)):currentST) statements
+    then buildSymbolTable ((name, typ):currentST) statements
 	else error ("Oh No! You already told me what " ++ name ++ " is!")
-buildSymbolTable currentST x = currentST
+buildSymbolTable currentST (x:xs) = buildSymbolTable currentST xs
 
+
+checkType :: Exp -> MType -> Bool
+checkType (Int x) MNum       = True
+checkType (Char x) MLett     = True
+checkType (String x) MSent   = True
+checkType _ _                = False
 --------------------------------------------------------------------------------
 
 
