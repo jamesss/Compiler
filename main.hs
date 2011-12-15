c398d87cc92d748f869608f94b1f2fc90fbe1d25
Alina Boghiu <ab3110@visual05.doc.ic.ac.uk>
2011-12-15 16:57:42 +0000

    I explained why the type checking doesn't actually take place, modified the main and added a line in the report, in the type checking section.

--- main.hs
+++ main.hs
@@ -8,7 +8,6 @@ import CodeGen
 
 main = do
     s <- getContents ;
-    --print (maliceParser (alexScanTokens s))
-    let st = buildSymbolTable [] (getInstr (maliceParser (alexScanTokens s)))
+    --print (buildSymbolTable [] (getInstr (maliceParser (alexScanTokens s))))
     putStr  (codeGen (getInstr (maliceParser (alexScanTokens s))) [] )
 
