573e398325ff509bb0d70f9698d528ac997fc66e
James Simpson <js2310@doc.ic.ac.uk>
2011-12-15 19:15:49 +0000

    Fixed incorrect bit about more than one action in do statement

--- main.hs
+++ main.hs
@@ -8,6 +8,6 @@ import CodeGen
 
 main = do
     s <- getContents ;
-    --print (buildSymbolTable [] (getInstr (maliceParser (alexScanTokens s))))
+    let st = (buildSymbolTable [] (getInstr (maliceParser (alexScanTokens s))))
     putStr  (codeGen (getInstr (maliceParser (alexScanTokens s))) [] )
 
